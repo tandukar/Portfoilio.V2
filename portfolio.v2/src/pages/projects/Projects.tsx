1f0d7e01dcba17d8eb79a1e2141c2bd4292f6442
tandukar <84461389+tandukar@users.noreply.github.com>
2023-12-26 23:13:07 +0545

    add alternating layout based on index

--- portfolio.v2/src/pages/projects/Projects.tsx
+++ portfolio.v2/src/pages/projects/Projects.tsx
@@ -19,28 +19,74 @@
 
 type ProjectsItemsProps = {
   href: string;
+  title: string;
+  description: string;
 };
-const ProjectsItems: React.FC<ProjectsItemsProps> = ({ href,title, description }) => {
+
+const ProjectsItems: React.FC<ProjectsItemsProps & { index: number }> = ({
+  href,
+  title,
+  description,
+  index,
+}) => {
+  console.log("Index:", index);
+
+  const isOdd = index % 2 !== 0;
+  const isImageOnLeft = isOdd;
+
   return (
     <a href={href}>
-      <div className="h-custom grid grid-cols-3 bg-white shadow-md rounded-3xl overflow-hidden">
-        <div className="h-full col-span-2  rounded-3xl py-6 px-6">
-          <div className="h-full bg-green-600"></div>
-        </div>
-        <div className="h-full py-5">
-          <h2 className="  font-bold text-slate-900 my-10">{title}</h2>
-          <p>{description}</p>
-        </div>
+      <div
+        className={`h-custom grid grid-cols-3 bg-white shadow-md rounded-3xl overflow-hidden ${
+          isImageOnLeft ? "flex-row" : "flex-row-reverse"
+        }`}
+      >
+        {isImageOnLeft ? (
+          <>
+            <div className="h-full py-5 pl-6">
+              <h2 className="font-bold text-slate-900 my-10">{title}</h2>
+              <p>{description}</p>
+            </div>
+            <div className="h-full col-span-2 rounded-3xl py-6 px-6">
+              <div className="h-full bg-green-600"></div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="h-full col-span-2 rounded-3xl py-6 px-6">
+              <div className="h-full bg-green-600"></div>
+            </div>
+            <div className="h-full py-5">
+              <h2 className="font-bold text-slate-900 my-10">{title}</h2>
+              <p>{description}</p>
+            </div>
+          </>
+        )}
       </div>
     </a>
   );
 };
+
 const Projects = () => {
   const projectItems = [
-    { href: "/#/projects", title: "Patient Management System", description: "This is a desciption for patient management service here in this system a hospital can manage patients " },
-    { href: "/#/projects", title: "Patient Management System", description: "This is a desciption for patient management service here in this system a hospital can manage patients " },
-    { href: "/#/projects", title: "Patient Management System", description: "This is a desciption for patient management service here in this system a hospital can manage patients " },
-
+    {
+      href: "/#/projects",
+      title: "Patient Management System",
+      description:
+        "This is a desciption for patient management service here in this system a hospital can manage patients ",
+    },
+    {
+      href: "/#/projects",
+      title: "Patient Management System",
+      description:
+        "This is a desciption for patient management service here in this system a hospital can manage patients ",
+    },
+    {
+      href: "/#/projects",
+      title: "Patient Management System",
+      description:
+        "This is a desciption for patient management service here in this system a hospital can manage patients ",
+    },
   ];
   return (
     <div className="bg-slate-50 py-16">
@@ -52,7 +98,7 @@ const Projects = () => {
         <div className="flex items-center justify-center">
           <div className="grid grid-rows w-3/5 gap-10">
             {projectItems.map((item, index) => (
-              <ProjectsItems key={index} {...item} />
+              <ProjectsItems key={index} index={index} {...item} />
             ))}
           </div>
         </div>
