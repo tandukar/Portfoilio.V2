744eaf702b93086f5b6f0cea1803f1c5ebec1fda
tandukar <84461389+tandukar@users.noreply.github.com>
2023-12-27 21:26:16 +0545

    Refactor ProjectsItems component layout with grid order

--- portfolio.v2/src/pages/projects/Projects.tsx
+++ portfolio.v2/src/pages/projects/Projects.tsx
@@ -37,31 +37,23 @@ const ProjectsItems: React.FC<ProjectsItemsProps & { index: number }> = ({
   return (
     <a href={href}>
       <div
-        className={`h-custom grid grid-cols-3 bg-white shadow-md rounded-3xl overflow-hidden ${
-          isImageOnLeft ? "flex-row" : "flex-row-reverse"
-        }`}
+        className={`h-custom grid grid-cols-3 bg-white shadow-md rounded-3xl overflow-hidden`}
       >
-        {isImageOnLeft ? (
-          <>
-            <div className="h-full py-5 pl-6">
-              <h2 className="font-bold text-slate-900 my-10">{title}</h2>
-              <p>{description}</p>
-            </div>
-            <div className="h-full col-span-2 rounded-3xl py-6 px-6">
-              <div className="h-full bg-green-600"></div>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="h-full col-span-2 rounded-3xl py-6 px-6">
-              <div className="h-full bg-green-600"></div>
-            </div>
-            <div className="h-full py-5">
-              <h2 className="font-bold text-slate-900 my-10">{title}</h2>
-              <p>{description}</p>
-            </div>
-          </>
-        )}
+        <div
+          className={`h-full ${
+            isImageOnLeft ? "col-span-2 order-2" : "col-span-2 order-last"
+          } rounded-3xl py-6 px-6`}
+        >
+          <div className="h-full bg-green-600"></div>
+        </div>
+        <div
+          className={`h-full ${
+            isImageOnLeft ? "col-span-1 order-last" : "col-span-1 order-1"
+          } py-5 pl-6`}
+        >
+          <h2 className="font-bold text-slate-900 my-10">{title}</h2>
+          <p>{description}</p>
+        </div>
       </div>
     </a>
   );
