e3c7646bfadd23968e391a6e8761cbf8ca66dd8f
tandukar <84461389+tandukar@users.noreply.github.com>
2024-01-04 22:30:28 +0545

    update to footer

--- portfolio.v2/src/pages/connect/Connect.tsx
+++ portfolio.v2/src/pages/connect/Connect.tsx
@@ -2,11 +2,11 @@ const Connect = () => {
   return (
     <div className="h-custom1 w-full mt-16">
       <div className="h-full w-full grid grid-rows-7 gap-4">
-        <div className="flex flex-col justify-center items-center font-bold text-slate-700">
-        <p className="text-teal-500 font-medium text-lg mb-4">
+        <div className="flex flex-col row-span-1 justify-center items-center font-bold text-slate-700">
+          <p className="text-teal-500 font-medium text-lg mb-4">
             04<span className="font-medium text-lg mb-10">. What’s Next?</span>
           </p>
-          <h1 className="text-5xl mb-10">Get in Touch</h1>
+          <h1 className="text-5xl mb-12">Get in Touch</h1>
           <p className="text-md text-center w-custom1 mb-10">
             Feel free to reach out if you have any questions, ideas, or just
             want to say hello! I'm looking forward to hearing from you.
@@ -15,10 +15,13 @@ const Connect = () => {
             Say Hello
           </button>
         </div>
-      
 
-        <div className="h-full w-96 row-span-3 flex flex-col justify-end items-center text-center text-sm font-bold text-slate-700 mx-auto ">
-          <p className="p-10">Designed & Built By me</p>
+        <div className="mt-28  w-full row-span-1 flex flex-col justify-center items-center text-center text-sm font-bold text-slate-700">
+          <p className="mb-5">Designed and Built By Pragyo Tandukar</p>
+
+          <div className="">
+            <p>&copy; 2024 Pragyo Tandukar. All rights reserved.</p>
+          </div>
         </div>
       </div>
     </div>
