283665c580e6891224742779850c3248c484e0e5
tandukar <84461389+tandukar@users.noreply.github.com>
2024-01-20 15:16:18 +0545

    update to skills section in about me

--- portfolio.v2/src/pages/aboutme/Aboutme.tsx
+++ portfolio.v2/src/pages/aboutme/Aboutme.tsx
@@ -3,6 +3,29 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { AiOutlineDatabase } from "react-icons/ai";
+import {
+  faReact,
+  faNodeJs,
+  faJs,
+  faPython,
+  faHtml5,
+  faCss3,
+  faBootstrap,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+const SkillBadge = ({ icon, text }: { icon: IconDefinition; text: string }) => {
+  return (
+    <div className="flex items-center m-2">
+      <FontAwesomeIcon icon={icon} size="2x" className="mr-2" />
+      <span className="text-white">{text}</span>
+    </div>
+  );
+};
+
 const Aboutme = () => {
   return (
     <div className="flex items-center justify-center text-justify h-screen ">
@@ -14,65 +37,59 @@ const Aboutme = () => {
           About Me
         </h2>
 
-        <VerticalTimeline lineColor={" #33BFA8"} layout="1-column-left">
+        <VerticalTimeline lineColor="#535C5A  " layout="1-column-left">
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{ background: "#115e59", color: "#33BFA8  " }}
+            contentStyle={{ background: "#115e59", color: "#ffff  " }}
             contentArrowStyle={{ borderRight: "7px solid  #115e59" }}
             iconStyle={{ background: "#33BFA8" }}
           >
-            <div className="vertical-timeline-element-title">
-              <p className="text-white ">Your Skills</p>
+            <div className="vertical-timeline-element-title font-bold">
+              Your Skills
             </div>
 
-            <div className="vertical-timeline-element-subtitle"></div>
-            <p className="text-white">
-              MERN, Redux Toolkit, Tailwind CSS, Python, Django, HTML, CSS,
-              Bootstrap, MySql, postgresql
-            </p>
+            <div className="vertical-timeline-element-subtitle">
+              <div className="flex flex-wrap justify-center">
+                <SkillBadge icon={faReact} text="MERN" />
+                <SkillBadge icon={faJs} text="Redux Toolkit" />
+                <SkillBadge icon={faJs} text="JavaScript" />
+                <SkillBadge icon={faNodeJs} text="Node.js" />
+                <SkillBadge icon={faPython} text="Python" />
+                <SkillBadge icon={faHtml5} text="HTML" />
+                <SkillBadge icon={faCss3} text="CSS" />
+                <SkillBadge icon={faBootstrap} text="Bootstrap" />
+              </div>
+            </div>
           </VerticalTimelineElement>
 
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{
-              background: "#115e59",
-
-              color: "#146833  ",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid #115e59" }}
+            contentStyle={{ background: "#115e59", color: "#ffff  " }}
+            contentArrowStyle={{ borderRight: "7px solid  #115e59" }}
             iconStyle={{ background: "#33BFA8" }}
-            date="Work Experience"
           >
-            <h2 className="vertical-timeline-element-title">
-              Your Work Experience
-            </h2>
+            Your Work Experience
+
+
+            
           </VerticalTimelineElement>
 
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{
-              background: "#115e59",
-
-              color: "#146833  ",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid #115e59" }}
+            contentStyle={{ background: "#115e59", color: "#ffff  " }}
+            contentArrowStyle={{ borderRight: "7px solid  #115e59" }}
             iconStyle={{ background: "#33BFA8" }}
-            date="Education"
           >
-            <h2 className="vertical-timeline-element-title">Your Education</h2>
+            Your Education
           </VerticalTimelineElement>
 
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{
-              background: "#115e59",
-              color: "#146833  ",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid #115e59" }}
+            contentStyle={{ background: "#115e59", color: "#ffff  " }}
+            contentArrowStyle={{ borderRight: "7px solid  #115e59" }}
             iconStyle={{ background: "#33BFA8" }}
-            date="Resume"
           >
-            <h2 className="vertical-timeline-element-title">Your Resume</h2>
+            Your Resume
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
