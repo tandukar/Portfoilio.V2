4639d18995ced501eb2f5e6e6b8cd75a9fc5f891
tandukar <84461389+tandukar@users.noreply.github.com>
2024-01-25 23:07:35 +0545

    update to education

--- portfolio.v2/src/pages/aboutme/Aboutme.tsx
+++ portfolio.v2/src/pages/aboutme/Aboutme.tsx
@@ -169,11 +169,11 @@ const Aboutme = () => {
             </div>
 
             <div className="vertical-timeline-element-subtitle pl-5  ">
-              <div className="flex-between flex-wrap mt-4">
+              <div className="flex-between flex-wrap mt-4 font-semibold">
                 Herald College Kathmandu (HCK)
               </div>
               <div className="flex-between flex-wrap">
-                <span className="text-sm ">BSc (Hons) Computer Science (BIT)</span>
+                <span className="text-sm  ">BSc (Hons) Computer Science (BIT)</span>
                 <span className="text-sm ml-10">2021 - 2023</span>
               </div>
             </div>
